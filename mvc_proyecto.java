ed3741619d8d11ff3768ad7c1096cc64d211e805
Joonaan <joonaan7@gmail.com>
2017-04-24 08:52:56 +0200

    Update mvc_proyecto.java

--- mvc_proyecto.java
+++ mvc_proyecto.java
@@ -1,10 +1,13 @@
 package mvc_proyecto;
 
-public class buscador{
-  public static void main(String[] args) {
-      ClaseModelo modelo = new ClaseModelo(); 
-		  ClaseVista vista = new ClaseVista();
 
-		  new ClaseControlador(vista, modelo); 
-  }
+public class ClasePrincipal {
+
+	public static void main(String args[]) {
+		ClaseModelo modelo = new ClaseModelo(); 
+		ClaseVista vista = new ClaseVista();
+
+		new ClaseControlador(vista, modelo); 
+	
+	} 
 }
