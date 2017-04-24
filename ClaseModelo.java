863d8b4c3f7f84c88a79afa267a28a065fed23eb
Joonaan <joonaan7@gmail.com>
2017-04-24 08:53:34 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,4 +1,20 @@
 package mvc_proyecto;
 
+import javax.swing.JOptionPane;
+
 public class ClaseModelo {
+	
+	
+	public void numero(int n1){
+		
+		int num=(int)(Math.random()*10+1);
+		
+		if(n1==num){
+			JOptionPane.showMessageDialog(null,"ENHORABUENA, HAS ACERTADO EL NUMERO.");		
+		}else{
+			JOptionPane.showMessageDialog(null,"MALA SUERTE, HAS FALLADO, EL NUMERO ERA "+num);
+		}
+	}
+		
 }
+		
