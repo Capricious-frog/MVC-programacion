0649a5e9a7b1c7eddc085f53dce83342be18bf1a
Joonaan <joonaan7@gmail.com>
2017-04-24 08:50:11 +0200

    Update ClaseVista.java

--- ClaseVista.java
+++ ClaseVista.java
@@ -1,32 +1,37 @@
 package mvc_proyecto;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Container; 
+import java.awt.FlowLayout; 
+import javax.swing.JButton; 
+import javax.swing.JFrame; 
+import javax.swing.JLabel; 
 import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JFrame;
-import java.awt.Button;
 
-public class ClaseVista extends JFrame implements ActionListener {
-  private static final long serialVersionUID = 1L;
-  
-  Button botonBuscador = new Button("Buscador");
-  
-  public ClaseVista() {
-      setLayout(new FlowLayout());
-      setSize(400, 130);
-      setLocationRelativeTo(null);
-      setTitle("Menú");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-      botonBuscador.addActionListener(this);
-      add(botonBuscador);
-		  setVisible(true);
-  }
-  
-  public void actionPerformed(ActionEvent e){
-    
-  }
+public class ClaseVista extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	Container bloqueContenedor = null;  
+	JButton botonNumero1 = null;
+	JButton botonNumero2 = null;
+	JButton botonNumero3 = null;
+
+	public ClaseVista() {
+		
+		bloqueContenedor = getContentPane(); 
+		setTitle("GRUPO 3"); 
+		setLayout(new FlowLayout()); 
+		setSize(800,500);
+
+		botonNumero1 = new JButton("BUSCADOR");
+		botonNumero2 = new JButton("PROGRAMA 2");
+		botonNumero3 = new JButton("PROGRAMA 3");
+
+
+		bloqueContenedor.add(botonNumero1);
+		bloqueContenedor.add(botonNumero2);
+		bloqueContenedor.add(botonNumero3);
+
+		setVisible(true);
+	}
 }
