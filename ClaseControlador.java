213f60e8f312e53c01f599aa6c9041b0bafb473a
Joonaan <joonaan7@gmail.com>
2017-04-24 08:51:38 +0200

    Update ClaseControlador.java

--- ClaseControlador.java
+++ ClaseControlador.java
@@ -1,4 +1,37 @@
 package mvc_proyecto;
 
-public class ClaseControlador extends JFrame {
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class ClaseControlador implements ActionListener {
+
+
+	ClaseModelo objModelo = null;
+	ClaseVista objVista = null;
+
+	public ClaseControlador(ClaseVista objVista, ClaseModelo objModelo) { 
+		this.objModelo = objModelo; 
+		this.objVista = objVista; 
+		actionListener(this);
+	}
+
+
+	@Override 
+	public void actionPerformed(ActionEvent objEvento) {
+		try {
+
+			
+		}
+		catch(Exception objExcepcion) {
+			objExcepcion.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	public void actionListener(ActionListener escuchador) {
+		objVista.botonNumero1.addActionListener(escuchador);
+	}
+
 }
